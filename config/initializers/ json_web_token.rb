1453c418182b48a6899534cbdfd3b7346b266bb0
Sukanta Kumar Mangal <skntmangal@gmail.com>
2020-09-04 11:58:55 +0530

    set secret key for production

--- config/initializers/ json_web_token.rb
+++ config/initializers/ json_web_token.rb
@@ -4,13 +4,12 @@ class JsonWebToken
   # Encodes and signs JWT Payload with expiration
   def self.encode(payload)
     payload.reverse_merge!(meta)
-    p "Rails.application.secrets.secret_key_base #{Rails.application.secrets.secret_key_base}"
-    JWT.encode(payload, Rails.application.secrets.secret_key_base)
+    JWT.encode(payload, ENV['SECRET_KEY_BASE'] || Rails.application.secrets.secret_key_base)
   end
 
   # Decodes the JWT with the signed secret
   def self.decode(token)
-    JWT.decode(token, Rails.application.secrets.secret_key_base)
+    JWT.decode(token, ENV['SECRET_KEY_BASE'] || Rails.application.secrets.secret_key_base)
   end
 
   # Validates the payload hash for expiration and meta claims
