21721d9b313d7ef23516e1d115dd0a729dba02fd
Sukanta Kumar Mangal <skntmangal@gmail.com>
2020-09-04 20:51:46 +0530

    add first_name/last_name in user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :user do
     email    { Faker::Internet.email }
     password { Faker::Internet.password(min_length: 6, max_length: 128) }
+    first_name { Faker::Name.name }
+    last_name { Faker::Name.name }
   end
 end
